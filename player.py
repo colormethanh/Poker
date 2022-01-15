5c15445c6b97c6d1c8e5c18f0d1a8e63763b9902
colormethanh <thnguyen1847@gmail.com>
2022-01-16 03:01:35 +0900

    Refactored some

--- player.py
+++ player.py
@@ -1,10 +1,11 @@
 class Player():
     """ A class to represent a plyer in a poker game"""
 
-    def __init__(self, username, ID, funds=0):
+    def __init__(self, username, ID):
         self.username = username
         self.hand = []
-        self.funds = 0
+        self.ledger = []
+        #self.funds = 0
         self.ID = ID
 
         print(
@@ -13,10 +14,35 @@ class Player():
 
     def __str__(self):
         return(
-            f"{self.username}\n"
-            f"Funds: ${self.funds}\n"
+            f"Username: {self.username}\n"
+            f"Funds: ${self.get_balance()}\n"
             f"ID: {self.ID}\n"
         )
 
-    def deposit(self, amount):
-        self.funds = amount + self.funds
+    def get_balance(self):
+        balance = sum([i.get("amount") for i in self.ledger])
+        return(balance)
+
+    def check_funds(self, amount):
+        if self.get_balance() >= amount:
+            return True
+        elif self.get_balance() < amount:
+            return False
+
+    def deposit(self, amount, description):
+        dep = {
+            "amount": amount,
+            "description": description
+        }
+        self.ledger.append(dep)
+
+    def widthdraw(self, amount, description):
+        if self.check_funds(amount):
+            act_widthdraw = {
+                "amount": int(f"-{amount}"),
+                "description": description
+            }
+            self.ledger.append(act_widthdraw)
+            return True
+        else:
+            return False
--- poker.py
+++ poker.py
@@ -1,12 +1,15 @@
 from game_stats import GameStats
 import game_functions as GF
 from cards import Cards
+from main_game import PokerGame
 
 
 def run():
     GS = GameStats()
     deck = Cards()
-    GF.home_screen(GS, deck)
+    poker_game = PokerGame(GS, deck)
+    GF.add_player(GS, p_name="thanh", ID="111", Funds=False)
+    GF.home_screen(GS, deck, poker_game)
 
 
 run()
